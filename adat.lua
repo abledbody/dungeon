3a77f70f62f80f2e3cb57a304995e170290e3e15
abledbody <drewisakid@gmail.com>
2019-11-19 16:14:48 -0500

    Cleaned adat code

--- adat.lua
+++ adat.lua
@@ -1,40 +1,43 @@
 --Animation Data
 --This file stores the timing and sprite data for each kind of animation
 
-local adat = {}
+local aData = {}
 
 --Timings--
 local timings = {}
-timings.bob = {0.6,0.06,0.6,0.06}
-timings.strike = {0.07,0.08,0.2}
-timings.twoIdle = {0.5,0.5}
+timings.bob =		{0.6,	0.06,	0.6,	0.06}
+timings.strike =	{0.07,	0.08,	0.2}
+timings.twoIdle =	{0.5,	0.5}
+
+--Frame data--
+local frames
 
 --Player--
-local fr = {}
-fr.idle = {
-	spr = {193,194,195,196},
-	timing = timings.bob}
-
-fr.attack = {
-	spr = {197,198,199},
-	offX = {-1,2,0},
-	timing = timings.strike}
-
-adat.player = fr
+aData.player = {
+    idle = {
+        spr = 	{193,	194,	195,	196},
+        timing = timings.bob
+    },
+    
+    attack = {
+        spr = 	{197,	198,	199},
+        offX = 	{-1,	2,		0},
+        timing = timings.strike
+    },
+}
 
 --Slime--
-fr = {}
-
-fr.idle = {
-	spr = {202,203},
-	timing = timings.twoIdle
+aData.slime = {
+	idle = {
+		spr = {202,203},
+		timing = timings.twoIdle
+    },
+    
+	attack = {
+		spr = 		{203,	202,	204,	203},
+		offX = 		{0,		1,		2,		1},
+		timing = 	{0.06,	0.06,	0.12,	0.06}
+	},
 }
-fr.attack = {
-	spr = {203,202,204,203},
-	offX = {0,1,2,1},
-	timing = {0.06,0.06,0.12,0.06}
-}
 
-adat.slime = fr
-
-return adat
+return aData
