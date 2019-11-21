aa3c88df3cf395b299377c57a3d812a02852c455
abledbody <drewisakid@gmail.com>
2019-11-21 17:07:16 -0500

    Separated diBox from main file

---
+++ diBox.lua
@@ -0,0 +1,96 @@
+local max = math.max
+local min = math.min
+
+--Parameters
+local boxTime = 1/20 --Should open in a 20th of a second.
+
+--Variables--
+local size = 0
+local targetSize = 6
+--Box vertical size increment in pixels per second
+local boxSpeed = 1
+local boxWidth = 200
+local message = {}
+
+local scrImage
+
+--Functions--
+--Returns an array of strings split up by newlines.
+local function lineSplit(text)
+	local lns = {}
+	
+	if text:sub(-1)~="\n" then
+		text=text.."\n"
+	end
+	
+	for s in text:gmatch("(.-)\n") do
+		table.insert(lns,s)
+	end
+	
+	return lns
+end
+
+--Gets the width of the widest string in an array
+local function textWidth(lns)
+	local lnsCount = #lns
+	local lineLens = {}
+	
+	for i = 1, lnsCount do
+		lineLens[i] = lns[i]:len()
+	end
+	
+	return max(unpack(lineLens))
+end
+
+--Dialogue box trigger
+local function show(m)
+	--We keep a screenshot of the game to display behind the text box.
+	local imgDat = screenshot()
+	scrImage = imgDat:image()
+	setState("diBox")
+	
+	SFX(9)
+	
+	size = 0
+	message = lineSplit(m)
+	boxWidth = textWidth(message)
+	
+	--Text characters are 4*6, with spaces of 1 pixel
+	targetSize = #message*7-1
+	boxWidth = boxWidth*5-1
+	boxSpeed = targetSize/boxTime
+end
+
+local function update(dt)
+	size = 
+		min(size+dt*boxSpeed,targetSize)
+end
+
+local function draw()
+	scrImage:draw()
+
+	local x = HSW+0.5-boxWidth/2
+	local y = HSH+0.5-size/2
+	
+	color(13)
+	rect(x-2,y-2,boxWidth+4,size+4)
+	color(0)
+	rect(x-1,y-1,boxWidth+2,size+2)
+	
+	if size >= targetSize then
+		color(7)
+		for i = 1, #message do
+			local ln = message[i]
+			local tw = ln:len()*5-1
+			print(ln,HSW+0.5-tw/2,y+(i-1)*7)
+		end
+	end
+end
+
+--Module--
+local diBox = diBox
+diBox.show = show
+diBox.update = update
+diBox.draw = draw
+
+return diBox
--- dungeon.lua
+++ dungeon.lua
@@ -34,43 +34,40 @@ local round = math.round
 --Modules--
 --These are all forward declarations of modules defined later,
 --so Lua can refer to these tables without needing to know what's in them.
-local diBox = {} --Dialogue box
-local gMap = {} --Game map
-local mobs = {} --Enemies, player, ect.
-local things = {} --Non-map objects within the game
+diBox = {} --Dialogue box
+gMap = {} --Game map
+mobs = {} --Enemies, player, ect.
+things = {} --Non-map objects within the game
 
 --Parameters--
 local camSmooth = 0.16
 
 --Constants--
-local SW,SH = screenSize()
-local HSW,HSH =
+SW,SH = screenSize()
+HSW,HSH =
 	SW/2,SH/2
-local DIRX = {-1,1,0,0}
-local DIRY = {0,0,-1,1}
+DIRX = {-1,1,0,0}
+DIRY = {0,0,-1,1}
 
 --Variables--
-local time = 0
-local pl --Player mob
+time = 0
+pl = nil --Player mob
 
 --The center of the camera in pixelspace
-local camX,camY = 480,48
+camX,camY = 480,48
 --The center of the room in gridspace
-local roomX,roomY = 0,0
+roomX,roomY = 0,0
 
 --Interaction indicated vertical position
 local indicatorY = 0
 
 --The last Examable looked at
-local lastLook
-local lookCount = 1
-
---Used to draw the game while it's paused
-local scrImage
+lastLook = nil
+lookCount = 1
 
 --Available game loops
-local updates = {}
-local draws = {}
+updates = {}
+draws = {}
 
 --Functions--
 --Returns the first position, but slightly closer to the
@@ -158,95 +155,7 @@ end
 ------------Dialogue Box--------------
 
 
-do
---Parameters
-local boxTime = 1/20 --Should open in a 20th of a second.
-
---Variables--
-local size = 0
-local targetSize = 6
---Box vertical size increment in pixels per second
-local boxSpeed = 1
-local boxWidth = 200
-local message = {}
-
---Functions--
---Returns an array of strings split up by newlines.
-local function lineSplit(text)
-	local lns = {}
-	
-	if text:sub(-1)~="\n" then
-		text=text.."\n"
-	end
-	
-	for s in text:gmatch("(.-)\n") do
-		table.insert(lns,s)
-	end
-	
-	return lns
-end
-
---Gets the width of the widest string in an array
-local function textWidth(lns)
-	local lnsCount = #lns
-	local lineLens = {}
-	
-	for i = 1, lnsCount do
-		lineLens[i] = lns[i]:len()
-	end
-	
-	return max(unpack(lineLens))
-end
-
---Dialogue box trigger
-local function show(m)
-	--We keep a screenshot of the game to display behind the text box.
-	local imgDat = screenshot()
-	scrImage = imgDat:image()
-	setState("diBox")
-	
-	SFX(9)
-	
-	size = 0
-	message = lineSplit(m)
-	boxWidth = textWidth(message)
-	
-	--Text characters are 4*6, with spaces of 1 pixel
-	targetSize = #message*7-1
-	boxWidth = boxWidth*5-1
-	boxSpeed = targetSize/boxTime
-end
-
-local function update(dt)
-	size = 
-		min(size+dt*boxSpeed,targetSize)
-end
-
-local function draw()
-	local x = HSW+0.5-boxWidth/2
-	local y = HSH+0.5-size/2
-	
-	color(13)
-	rect(x-2,y-2,boxWidth+4,size+4)
-	color(0)
-	rect(x-1,y-1,boxWidth+2,size+2)
-	
-	if size >= targetSize then
-		color(7)
-		for i = 1, #message do
-			local ln = message[i]
-			local tw = ln:len()*5-1
-			print(ln,HSW+0.5-tw/2,y+(i-1)*7)
-		end
-	end
-end
-
---Module--
-diBox.show = show
-diBox.update = update
-diBox.draw = draw
-
-end
+diBox = dofile("D:/dungeon/diBox.lua")
 
 
 -----------------Map------------------
@@ -761,7 +670,6 @@ end
 
 draws.diBox = function(dt)
 	clear()
-	scrImage:draw()
 	diBox.draw()
 end
 
